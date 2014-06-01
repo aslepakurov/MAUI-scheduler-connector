cb105f768efe43eb4512b980d86683fd45740a91
barabashka <ericbarashkin@gmail.com>
2014-06-01 16:57:55 +0300

    added status field

--- src/main/java/ua/kpi/comsys/maui/domain/Request.java
+++ src/main/java/ua/kpi/comsys/maui/domain/Request.java
@@ -20,12 +20,15 @@ public class Request {
 
     public static final double DEFAULT_PROTOCOL_VERSION = 0.5;
 
+    //TODO: Map fields in camelCase, return and insert in under_score
     @Id
     private String id;
     @Field("name")
     private String name;
     @Field("priority")
     private int priority;
+    @Field("status")
+    private Status status;
     @Field("classId")
     private ClassID classId;
     @Field("command")
@@ -43,6 +46,7 @@ public class Request {
     public Request(String id, String name, ClassID classId, String user_id, String email, int priority)  {
         this.id = id;
         this.name = name;
+        this.status = Status.CREATED;
         this.email = email;
         this.priority = priority;
         this.classId = classId;
@@ -83,6 +87,10 @@ public class Request {
         return creation_date;
     }
 
+    public Status getStatus() {
+        return status;
+    }
+
     public void setId(String id) {
         this.id = id;
     }
@@ -113,5 +121,9 @@ public class Request {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
---
+++ src/main/java/ua/kpi/comsys/maui/domain/Status.java
@@ -0,0 +1,8 @@
+package ua.kpi.comsys.maui.domain;
+
+/**
+ * Created by barabashka on 6/1/2014.
+ */
+public enum Status {
+    CREATED, PROCESSING, COMPLETE, FAILED;
+}
