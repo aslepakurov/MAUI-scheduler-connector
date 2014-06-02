935699ea75d94a0fb256cd107d9c751f1ed90c00
barabashka <ericbarashkin@gmail.com>
2014-06-02 15:29:07 +0300

    enum + methode

--- src/main/java/ua/kpi/comsys/maui/domain/Status.java
+++ src/main/java/ua/kpi/comsys/maui/domain/Status.java
@@ -4,5 +4,14 @@ package ua.kpi.comsys.maui.domain;
  * Created by barabashka on 6/1/2014.
  */
 public enum Status {
-    CREATED, PROCESSING, COMPLETE, FAILED;
+    CREATED("created"),
+    PROCESSING("processing"),
+    COMPLETE("complete"),
+    FAILED("failed");
+
+    private final String name;
+
+    private Status(String name) {
+        this.name = name;
+    }
 }
