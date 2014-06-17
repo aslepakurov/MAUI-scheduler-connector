8114fb462b07f7284cf73d76ffcbbc8970b206ab
barabashka <ericbarashkin@gmail.com>
2014-06-17 04:23:25 +0300

    route

--- src/main/java/ua/kpi/comsys/maui/route/RequestRoute.java
+++ src/main/java/ua/kpi/comsys/maui/route/RequestRoute.java
@@ -2,6 +2,7 @@ package ua.kpi.comsys.maui.route;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * MAUIService Class
@@ -10,8 +11,12 @@ import org.apache.camel.builder.RouteBuilder;
  * @version 6/17/2014
  */
 public class RequestRoute extends RouteBuilder {
+    @Value("${rest.port}")
+    private String port;
+    @Value("${rest.root}")
+    private String root;
     @Override
     public void configure() throws Exception {
-        from("cxfrs://bean//mauiREST/").log(LoggingLevel.INFO, "--------!!!!!SUPER WORKING CXF!!!!!--------");
+        from("jetty:http://0.0.0.0:"+port+"/"+root+"/maui/status").log(LoggingLevel.INFO, "--------!!!!!SUPER WORKING CXF!!!!!--------");
     }
 }
