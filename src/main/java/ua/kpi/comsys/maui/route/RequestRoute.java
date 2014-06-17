31a20ddcb7d13a54d6a469ab646749103e9e3a16
barabashka <ericbarashkin@gmail.com>
2014-06-17 03:55:19 +0300

    going cxf

---
+++ src/main/java/ua/kpi/comsys/maui/route/RequestRoute.java
@@ -0,0 +1,17 @@
+package ua.kpi.comsys.maui.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * MAUIService Class
+ *
+ * @author Andrew S. Slepakurov
+ * @version 6/17/2014
+ */
+public class RequestRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("cxfrs://bean//mauiREST/request").log(LoggingLevel.INFO, "--------!!!!!SUPER WORKING CXF!!!!!--------");
+    }
+}
